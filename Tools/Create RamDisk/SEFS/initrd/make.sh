e57c57194d6760b4fe3c300cbf539ac4b98e092d
NDRAEY <pikachu_andrey@vk.com>
2022-11-26 17:34:51 +0900

    Restore

--- Tools/Create RamDisk/SEFS/initrd/make.sh
+++ Tools/Create RamDisk/SEFS/initrd/make.sh
@@ -1,1 +1,2 @@
 nasm -felf32 start.asm && ld.lld-13 start.o -o start
+nasm -felf32 justexit.asm && ld.lld-13 justexit.o -o justexit
--- Tools/Create RamDisk/SEFS/initrd/start.asm
+++ Tools/Create RamDisk/SEFS/initrd/start.asm
@@ -1,6 +1,12 @@
-global main
-main:
-	mov eax, 0x02
+section .text
+global _start
+_start:
+	mov eax, 0x03
+	mov ebx, 0x00
+	mov edx, msg
 	int 0x50
 	
 	ret
+
+section .data
+msg: db "Hello World!!!", 0
