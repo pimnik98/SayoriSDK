ee0926dc006e5756d950ac2e3f16c3b1eec56b5a
pimnik98 <nikita.piminoff@yandex.ru>
2022-12-08 20:56:36 +0300

    📦 [ПО] Исправления для скрипта сборки и генерации make_initrd.sh

--- Tools/Create RamDisk/SEFS/make_initrd.sh
+++ Tools/Create RamDisk/SEFS/make_initrd.sh
@@ -1,6 +1,13 @@
+#!/bin/bash
+BASEDIR=`dirname $0`
+PROJECT_PATH=`cd $BASEDIR; pwd`
+
+#echo 'Using path: ' $PROJECT_PATH
+
 echo "--------- Building Programs"
-cd initrd && bash make.sh && cd ..
+cd initrd && bash make.sh
 echo "--------- Building Initrd Builder"
+cd "$PROJECT_PATH"
 make
 echo "--------- Packing..."
-./scrd --dir initrd
+./scrd --dir ./initrd
