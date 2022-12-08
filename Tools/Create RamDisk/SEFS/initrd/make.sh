35a18b61d16590f1ac804a20084d4b640003e745
pimnik98 <nikita.piminoff@yandex.ru>
2022-12-08 21:01:49 +0300

    📦 [ПО] Проверка наличие NASM при сборке программ (make_initrd)

--- Tools/Create RamDisk/SEFS/initrd/make.sh
+++ Tools/Create RamDisk/SEFS/initrd/make.sh
@@ -1,7 +1,12 @@
+#!/bin/bash
 echo "    --------- C Programs"
 cd test
 make
 cd ..
 echo "    --------- ASM Programs"
+if ! [ -x "$(command -v nasm)" ]; then
+  echo 'Error: nasm is not installed.' >&2
+  exit 1
+fi
 nasm -felf32 start.asm && ld.lld-13 -Tlink.ld start.o -o start
 nasm -felf32 justexit.asm && ld.lld-13 -Tlink.ld justexit.o -o justexit
