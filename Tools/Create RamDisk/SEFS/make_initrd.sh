0330bdefef5f7c01497358d4875c312d482c4507
NDRAEY <andrejpavlenko666@gmail.com>
2023-06-05 16:35:20 +0900

    update

--- Tools/Create RamDisk/SEFS/make_initrd.sh
+++ Tools/Create RamDisk/SEFS/make_initrd.sh
@@ -7,15 +7,9 @@ PROJECT_PATH=`cd $BASEDIR; pwd`
 
 #echo 'Using path: ' $PROJECT_PATH
 
-<<<<<<< HEAD
-echo "--------- Building Programs"
-cd initrd && bash make.sh
-rm initrd/start.o || true
-=======
 # echo "--------- Building Programs"
 # cd initrd && bash make.sh
 # rm initrd/start.o || true
->>>>>>> 6b40875c0261107c794fdef5c1ec27acfe84a01e
 echo "--------- Building Initrd Builder"
 cd "$PROJECT_PATH"
 make
