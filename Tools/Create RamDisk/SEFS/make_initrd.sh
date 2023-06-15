f9be09e0e6174777e3022872fae33cde3e085c93
NDRAEY <andrejpavlenko666@gmail.com>
2023-06-15 18:10:09 +0900

    Try

--- Tools/Create RamDisk/SEFS/make_initrd.sh
+++ Tools/Create RamDisk/SEFS/make_initrd.sh
@@ -5,11 +5,10 @@ set -e
 BASEDIR=`dirname $0`
 PROJECT_PATH=`cd $BASEDIR; pwd`
 
-#echo 'Using path: ' $PROJECT_PATH
+echo 'Using path: ' $PROJECT_PATH
 
-# echo "--------- Building Programs"
-# cd initrd && bash make.sh
-# rm initrd/start.o || true
+echo "--------- Building Programs"
+bash make_programs.sh
 echo "--------- Building Initrd Builder"
 cd "$PROJECT_PATH"
 make
