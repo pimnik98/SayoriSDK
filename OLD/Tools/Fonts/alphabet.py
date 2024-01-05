af808165865a6c421bad30e5493537ac293b1884
New Year Maractus <pikachu_andrey@vk.com>
2024-01-05 15:03:10 +0900

    SDK: Remove deprecated files

--- OLD/Tools/Fonts/alphabet.py
+++
@@ -1,9 +0,0 @@
-ENG = ''.join([chr(i) for i in range(ord('A'), ord('Z')+1)])
-ENG_low = ENG.lower()
-
-RUS = "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯ"
-RUS_low = RUS.lower()
-
-SYMBOLS = ".,!№;%:?*()_+-=@#$^&[]{}<>|\\/~"
-
-Alphabet = ENG + ENG_low + RUS + RUS_low + SYMBOLS
