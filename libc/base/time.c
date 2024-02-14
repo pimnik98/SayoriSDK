c9639853513961f06d69807b19a69eec3384dcca
NDRAEY <pikachu_andrey@vk.com>
2024-02-14 09:57:12 +0900

    Date: Add date/time syscall

---
+++ libc/base/time.c
@@ -0,0 +1,19 @@
+//
+// Created by ndraey on 22.01.24.
+//
+
+#include "../include/time.h"
+#include "syscall.h"
+
+sayori_time_t get_time() {
+	sayori_time_t time;
+	
+    _syscall(
+            DATETIME_SYSCALL,
+            &time,
+            0,
+            0
+            );
+
+   	return time;
+}
