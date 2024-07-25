c02367604fcc469969d6460e485882daebb1d6b9
NDRAEY <pikachu_andrey@vk.com>
2024-07-25 22:33:04 +0900

    C: Add malloc_align()

--- libc/include/memory.h
+++ libc/include/memory.h
@@ -4,7 +4,10 @@
 #define MEMORY_FREE_SYSCALL 2
 #define MEMORY_REALLOC_SYSCALL 18
 
+#include "stdint.h"
+
 void* realloc(void* memory, size_t size);
 void* calloc(size_t size, size_t count);
+void* malloc_align(size_t size, size_t align);
 void* malloc(size_t size);
 void free(void* ptr);
